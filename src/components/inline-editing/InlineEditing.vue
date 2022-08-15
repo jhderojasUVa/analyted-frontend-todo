a063896b19000852d63ca8fa1c596f30e0da175b
Jesus A Hernandez de Rojas <jhderojas@gmail.com>
2022-08-15 12:18:06 +0100

    feat(): methods completed

---
+++ src/components/inline-editing/InlineEditing.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    name: 'InlineEditing',
+    props: {
+        data: String
+    },
+    mounted() {
+        // set the focus on the description
+        // so when moving out we emit the changes
+        this.$refs.description.focus();
+    },
+    methods: {
+        updateText() {
+            // emit a change on the description
+            this.$emit('descriptionChanged', this.data);
+        }
+    }
+}
+</script>
+
+<template>
+    <input ref="description" type="text" v-model="data" v-on:blur="updateText" size="20" maxlength="50" />
+</template>
+
+<style scoped>
+    input {
+        padding: 0.5em;
+        border-radius: 3px;
+        border: 1px solid rgba(0, 0, 0, 0.5);
+    }
+</style>
---
+++ src/components/list-todos/ListTodo.vue
@@ -0,0 +1,59 @@
+<script>
+import TodoItem from '../todo-item/TodoItem.vue';
+
+export default {
+    name: 'ListTodo',
+    props: {
+        todoItems: Array
+    },
+    watch: {
+        todoItems: (newVal, oldVal) => {
+            console.log(newVal);
+        }
+    },
+    methods: {
+        deleteTodo(id) {
+            // pass to parent
+            this.$emit('deleteTodo', id);   
+        },
+        descriptionChanged(data) {
+            // pass to parent
+            this.$emit('descriptionChanged', data);
+        }
+    },
+    components: { 
+        TodoItem 
+    },
+    emits: [ 'deleteTodo', 'descriptionChanged' ]
+}
+</script>
+
+<template>
+    <ul>
+        <li v-for="item in todoItems">
+         <TodoItem :data="item" @deleteTodo="deleteTodo" @descriptionChanged="descriptionChanged"/>
+        </li>
+    </ul>
+</template>
+
+<style scoped>
+    ul {
+        margin: 0;
+        padding: 0;
+    }
+
+    li {
+        list-style-type: none;
+        margin: 0.5em 0em;
+        padding: 1em;
+        border-radius: 3px; 
+    }
+
+    li:nth-child(even) {
+        background-color: rgba(0, 0, 0, 0.05);
+    }
+
+    li a {
+        cursor: pointer;
+    }
+</style>
