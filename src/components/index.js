32b1890d1427163c8789535df1d8ef53db042399
Jesus A Hernandez de Rojas <jhderojas@gmail.com>
2022-08-16 15:59:53 +0100

    feat(): finish edit

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export * from './add-todo/Add-todo.vue';
+export * from './inline-editing/InlineEditing.vue';
+export * from './list-todos/ListTodo.vue';
+export * from './principal/Principal.vue';
+export * from './todo-item/TodoItem.vue';
