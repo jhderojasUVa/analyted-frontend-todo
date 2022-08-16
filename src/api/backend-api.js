44594b38be658bd29049b28a4c69cbc99a08b357
Jesus A Hernandez de Rojas <jhderojas@gmail.com>
2022-08-16 15:59:53 +0100

    feat(): finish edit

--- src/api/backend-api.js
+++ src/api/backend-api.js
@@ -35,15 +35,6 @@ const getOneTodo = async (id) => {
 // adds a new todo
 const addNewTodo = async (description) => {
     try {
-        const kaka = (await axiosInstance({
-            url: 'http://localhost:8080/',
-            method: 'post',
-            data: {
-                description,
-                completed: false,
-            }
-        }))
-        console.log(kaka)
         return (await axiosInstance({
             url: 'http://localhost:8080/',
             method: 'post',
@@ -80,6 +71,17 @@ const removeTodo = async (id) => {
 // always deconstruct the object for easy use
 const changeTodo = async ({ id = '', description = '', completed = false } = {}) => {
     try {
+        console.log({ id, description, completed })
+        const kaka = (await axiosInstance({
+            url: `http://localhost:8080/${id}`,
+            method: 'put',
+            data: {
+                description,
+                completed,
+            }
+        }));
+
+        console.log(kaka)
         return (await axiosInstance({
             url: `http://localhost:8080/${id}`,
             method: 'put',
